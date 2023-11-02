c0f97ab9bbb236a3adef9d9fd27286cf584c3d9d
RPrathiba17 <111226293+RPrathiba17@users.noreply.github.com>
2023-11-02 18:16:33 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,23 @@
+print("creating a string:")
+s="goodmorning"
+print(s)
+print("indexing:")
+print(s[1])
+print(s[0])
+print(s[6])
+print("slicing:")
+print(s[2:])
+print(s[7:])
+print(s[:4])
+print("concatenation:")
+print(s+" prathiba")
+print(s+" have ur coffee")
+S=s+" prathiba"
+print(S)
+print("repetition:")
+print(s*2)
+print(S*2)
+print("membership operator:(in/not in):")
+s="good morning"
+print("m"in s)
+print("a"not in s)
